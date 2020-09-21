597772014f37b9496bcc9a53ae248dbbe52c377b
maerd19 <revolutions_333@hotmail.com>
2020-09-20 21:47:53 -0500

    Agregando React Router DOM y componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Header from "./components/Header";
+import Productos from "./components/Productos";
+import NuevoProducto from "./components/NuevoProducto";
+import EditarProducto from "./components/EditarProducto";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h2>CRUD</h2>
-    </div>
+    <Router>
+      <Header />
+
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Productos} />
+          <Route exact path="/productos/nuevo" component={NuevoProducto} />
+          <Route
+            exact
+            path="/productos/editar/:id"
+            component={EditarProducto}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
